bc2acdba64bdbd57da99be46b3a53ef175cc5e89
Wolfgang Albrecht <wolfgang.albrecht@ok.de>
2015-11-14 15:06:45 +0100

    update

--- sketch.js
+++ sketch.js
@@ -8,11 +8,23 @@ var temp_y3 = 0;
 var temp_x4 = 0;
 var temp_y4 = 0;
 
+var w = width;
+var h = height;
+
+
 function setup(){
 
+canvas.parent("container");
 
-createCanvas(800,600);
-background(0);
+frameRate(6);
+
+colorMode(HSB,360,100,100,100);
+createCanvas(1000,800);
+background(267,90,78);
+
+}
+
+function coordinates (){
 
 temp_x1 = random(0, width/2);
 temp_y1 = random(0,height/2);
@@ -24,27 +36,31 @@ temp_x3 = random(width/2+1, width);
 temp_y3 = random(height/2+1,height);
 }
 
+
 function draw() {
-colorMode(HSB,360,100,100,100);
 
-//pointing legs in opposite qudrant?
-if (temp_y1>temp_y2 && temp_x1>temp_x4) { //how to combine conditions
-	stroke(0, 100,100, 100);//red
-}	else{
-	stroke(130, 100, 100, 100);//green
+coordinates();
+
+if ( temp_x1 > width/4 && temp_x3 < 3*width/4){
+
+
+	stroke(47,87,84);
+	fill(0,100,100,7);
 }
-//is left leg close to the half width
-// if (temp_x1 > width/4 temp_x4 > width/4){ 
-
-// 	stroke(240, 100,100, 100);//blue
 
 
+fill(180,100,100,7);
 
 
-strokeWeight(5);
-noFill();
-	
+quad();
 
+function quad (){
+
+
+
+
+
+strokeWeight(1);
 beginShape();
 vertex(temp_x1, temp_y1);
 vertex(temp_x2, temp_y2);
@@ -52,5 +68,11 @@ vertex(temp_x3, temp_y3);
 vertex(temp_x4, temp_y4);
 endShape(CLOSE);
 
-
 }
+}
+	
+
+
+
+
+
